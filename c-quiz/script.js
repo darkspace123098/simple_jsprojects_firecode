1f8555897748d29913d8f795202672ec9c307c43
Mohammed Ali <mohammedali777444@gmail.com>
2025-06-04 16:37:49 +0530

    question update

--- c-quiz/script.js
+++ c-quiz/script.js
@@ -1,8 +1,8 @@
 const questions = [
     {question:"who is the father of computer ",option:["Charles Mcgill","Arman Traut","Charles Babage","Branden Eich"],correct:2},
-    {question:"",option:[1,2,3,4],correct:2},
-    {question:"e",option:[1,2,3,4],correct:1},
-    {question:"f",option:[1,2,3,4],correct:0}
+    {question:"javascript is founded by",option:["james gosling","b jarne straustrap","brenden eich","ada lovlace"],correct:2},
+    {question:"which is the engine that runs js",option:["v6","v8","v8.0","v6.5"],correct:1},
+    {question:"nodejs is a ",option:["framework","library","compiler","run time env"],correct:3}
 ]
 
 let current = 0
