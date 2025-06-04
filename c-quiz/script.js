e3ed23c771bfac2bdc54959c6a41495e76e4dc58
Mohammed Ali <mohammedali777444@gmail.com>
2025-06-04 17:54:53 +0530

    attempt

--- c-quiz/script.js
+++ c-quiz/script.js
@@ -1,3 +1,4 @@
+
 const questions = [
     {question:"who is the father of computer ",option:["Charles Mcgill","Arman Traut","Charles Babage","Branden Eich"],correct:2},
     {question:"javascript is founded by",option:["james gosling","b jarne straustrap","brenden eich","ada lovlace"],correct:2},
@@ -7,15 +8,44 @@ const questions = [
 
 let current = 0
 let score = 0
+let attempt =3
+
+function validate() {
+let quizId = document.querySelector('#quiz_id').value
+
+    if(quizId=="101")
+    {
+          loadQuestion()
+
+    }
+    else{
+        alert("You have not yet registered !!")
+
+    }
+}
 
 function loadQuestion(){
     let q = questions[current]
+    let load = document.querySelector('.box')
+
+    load.innerHTML = `<header>
+            <h1 class="head"><center>Quiz App</center></h1>
+        </header>
+        
+        <h2 id="question">Question....</h2>
+        <div id="options"></div>
+        <div class="btn">
+            <button onclick="nextQuestion()">Submit</button>
+            <button onclick="viewScore()">Score</button>
+        </div>
+`
     document.getElementById('question').textContent =q.question
     optionHtml = ""
     for (let i = 0; i< q.option.length; i++) {
         optionHtml+=`<label><input type="radio"class="opt" name="choice" value="${i}" />${q.option[i]}</label><br>`
         document.getElementById('options').innerHTML = optionHtml
     } 
+
 }
 function nextQuestion() {
     let selected = document.querySelector('input[name="choice"]:checked')
@@ -36,11 +66,26 @@ function nextQuestion() {
 
        document.querySelector('.box').innerHTML=`
         <h2>You Scored ${score} out of ${questions.length}</h2>
-        <button>Play Again</button>
+        <h3>Attempt Left : ${attempt}</h3>
+        <div class="btn">
+        <button  onclick="playAgain()">Play Again</button>
+        </div>
         `
     }
 }
 function viewScore(){
     alert("Your Score is "+score)
 }
-loadQuestion()
+function playAgain()
+{
+    current = 0
+    attempt--
+    if(attempt>0){
+      loadQuestion()
+    }
+    else{
+        alert("You already attempted 3 times")
+
+    }
+}
+// loadQuestion()
